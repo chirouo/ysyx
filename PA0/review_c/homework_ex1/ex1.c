10bb1622423abc1e406d1886d332f2eebbb41fea
gx <2627403974@qq.com>
2024-11-22 10:00:45 +0800

    11.21 ex0-ex4

---
+++ PA0/review_c/homework_ex1/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+	printf("1---\n");
+	printf("2---\n\r");
+	printf("3---\r\n");
+	printf("0---\r");
+    return 0;
+}
