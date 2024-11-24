0b43067bb8139f4b84d9dbb4058032d772b3db86
gx <2627403974@qq.com>
2024-11-24 21:50:36 +0800

    have completed ex17----Database: get set delete list, create still need think

--- PA0/review_c/homework_ex17/ex17.c
+++ PA0/review_c/homework_ex17/ex17.c
@@ -6,7 +6,6 @@
 #define MAX_ROWS 100
 
 typedef struct Address{
-	int id;
 	int set;
 	char name[MAX_DATA];
 	char email[MAX_DATA];
@@ -25,28 +24,29 @@ typedef struct Connection{
 void Database_loader(Database *data, FILE *file);
 void die(const char* message)
 {
-	if (errno){
+	if (errno) {
 		perror("systeme error111");
-	}else{
+	}else {
 		puts(message);		
 	}	
 	exit(1);
 }
-Connection* Database_open(char *filename, char *action)
+Connection* Database_open(char *filename, char action)
 {
 	Connection *conn = malloc(sizeof(Connection));
-	if(!conn) die("conn cannot malloc");
-	if(action == "c")
-	{
-		conn->file = fopen(filename, "w");
-		if(!conn->file) die("the file dose not exist");
-	}
-	else{
+	if(!conn) die("conn cannot malloc"); 
+	if(action == 'g') {
 		conn->file = fopen(filename, "r");
-		if(!conn->file) die("the file dose not exist");
+	}else {
+		conn->file = fopen(filename, "r+");
+	}
+	if(!conn->file) {
+		die("the file dose not exist");
 	}
 	conn->data = malloc(sizeof(Database));
-	if (!conn->data) die("conn->data cannot malloc");
+	if (!conn->data) {
+		die("conn->data cannot malloc");
+	}
 	Database_loader(conn->data, conn->file);
 	return conn;
 } 
@@ -54,40 +54,110 @@ void Database_loader(Database *data, FILE *file)
 {
 	rewind(file);
 	int res = fread(data, sizeof(Database), 1, file);
-	if (res == -1) die("Database_loader fread error");
+	if (res == -1) {
+		die("Database_loader fread error");
+	}
 }
 void Database_create(Connection *conn)
 {
 }
-void Address_print(Address *info)
+void Address_print(Address *info, int id)
 {
-	printf(info->
+	printf("id: %d\tname: %s\temail: %s\n\r", id, info->name, info->email);
+}
+Address* Database_get(Connection * conn, int id)
+{
+	int i = 0;
+	Address *the_find = malloc(sizeof(Address));
+	if (conn->data->address[id].set == 1) {
+		the_find = &conn->data->address[id];
+		return the_find;
+	}
+	free(the_find);
+	return NULL;
+}
+void Database_write(Connection *conn)
+{
+	rewind(conn->file);
+	int res = fwrite(conn->data, sizeof(Database), 1, conn->file);
+}
+
+void Database_set(Connection *conn, int id, char *name, char *email)
+{
+	if (Database_get(conn, id)) {
+		die("this address data have existed! please try another one to set!");
+	}
+	strcpy(conn->data->address[id].name, name);
+	strcpy(conn->data->address[id].email, email);
+	conn->data->address[id].set = 1;
+	Database_write(conn);
+	printf("set %d successful!\n\r", id);
+}
+
+void Database_list(Connection *conn)
+{
+	int count = 0; 
+	for (int i = 0; i < MAX_ROWS; ++ i)
+	{
+		if (conn->data->address[i].set == 1) {
+			Address_print(&conn->data->address[i], i);
+			count++;
+		}
+	}
+	if (count == 0) {
+		printf("sorry there is no data existed~\n\r");
+	}
+}
+void Database_delete(Connection * conn, int id)
+{
+	if (!Database_get(conn, id)) {
+		die("this address data didnt existet! please try another one to delete!");
+	}else {
+		conn->data->address[id].set = 0;
+		printf("delete %d successful!\n\r", id);
+	}
+}
 int main(int argc, char* argv[])
 {
-	if (argc < 3) die("lack args!");	
+	if (argc < 3) {
+		die("lack args!");	
+	}
 	char* filename = argv[1];
 	char action = argv[2][0];
 	Connection *conn = Database_open(filename, action); 
-	if (argc > 3){
-		int id =  atoi(argv[3]);
+	int id = 0;
+	if (argc > 3) {
+		id =  atoi(argv[3]);
 	}	
 	switch (action){
 	case 'c':
 		Database_create(conn);
-		printf("c\n");
 		break;
 	case 'g':
-		printf("g\n");
+		if (argc < 4) {
+			die("lack the arg of id!");
+		}
+		Address * find_one = Database_get(conn, id);
+		if (find_one) {
+			Address_print(find_one, id);
+		}else {
+			die("didnt find this data");
+		} 
 		break;
 	case 's':
-		printf("s\n");
+		if (argc < 6) {
+			die("lack the arg of the address data to insert!");
+		}
+		Database_set(conn, id, argv[4], argv[5]);
 		break;
 	case 'd':
-		printf("d\n");
+		Database_delete(conn, id);
 		break;
 	case 'l':
-		printf("l\n");
+		Database_list(conn);
 		break;
+	default:
+		die("please input the operation!");
 	}	
 	return 0;
-
+}
