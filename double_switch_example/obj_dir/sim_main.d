3e6ca91ca164260a65937b16c22b4fec9673b86d
gx <2627403974@qq.com>
2024-11-26 21:31:49 +0800

    complete the simulation and learn electricity theory

--- double_switch_example/obj_dir/sim_main.d
+++ double_switch_example/obj_dir/sim_main.d
@@ -3,4 +3,9 @@ sim_main.o: ../sim_main.cpp Vtop.h \
  /usr/local/share/verilator/include/verilatedos.h \
  /usr/local/share/verilator/include/verilated_config.h \
  /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
+ /usr/local/share/verilator/include/verilated_funcs.h \
+ /usr/local/share/verilator/include/verilated_fst_c.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilated_trace.h \
+ /usr/local/share/verilator/include/verilated_trace_defs.h \
+ /usr/local/share/verilator/include/gtkwave/fstapi.h
--- double_switch_example/sim_main.cpp
+++ double_switch_example/sim_main.cpp
@@ -3,19 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "verilated_fst_c.h"
 int main(int argc, char **argv) {
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     Vtop *top = new Vtop{contextp};
+
+    //waveforms
+    Verilated::traceEverOn(true);
+    VerilatedFstC* tfp = new VerilatedFstC;
+    top->trace(tfp, 99);
+    Verilated::mkdir("logs");
+    tfp->open("logs/simx.fst");
+    
     while (!contextp->gotFinish()) {
         int a = rand() & 1;
         int b = rand() & 1;
         top->a = a;
         top->b = b;
+        contextp->timeInc(1);
         top->eval();
+        tfp->dump(contextp->time());
         printf("a = %d, b = %d, f = %d\n", a, b, top->f);
         assert(top->f == (a ^ b));
     }
+    tfp->close();
+    delete tfp;
     delete top;
     delete contextp;
     return 0;
