bcf668f34d7fdf7186f8f9f3e811405edd5ed7e5
gx <2627403974@qq.com>
2024-12-01 20:45:08 +0800

    Debug c on vscode base on .vscode/xx.json Makefile
    based on  	.vscode/task.json(for build c)
    			.vscode/launch.json(for launching the debug executable program,
    								before launching, the build task must be done,
    			Project-specific Makefile(have to write it by yourself)
    PS:	the folder ".vscode" is under the workspacefolder----"~/ysyx", But I backed it up under homework_ex32, you can open .vocode to read these "based" xx.json files  cause I can't guarantee that the json file in the.vscode folder under the ~/ysyx working directory will still be correct

---
+++ review_c/homework_ex32/liblcthw/src/lcthw/list.h
@@ -0,0 +1,41 @@
+#ifndef _lcthw_List_h
+#define _lcthw_List_h
+#include <stdlib.h>
+typedef struct ListNode
+{
+    struct ListNode *next;
+    struct ListNode *prev;
+    void *val;
+} ListNode;
+typedef struct List
+{
+    int count;
+    ListNode *first;
+    ListNode *last; 
+} List;
+
+#define List_count(A) (A->count)
+#define List_first(A) (A->first != NULL ? A->first->val : NULL)
+#define List_last(A) (A->last != NULL ? A->last->val : NULL)
+
+List *List_create();
+void List_clear(List *list);    // Clear ListNode 's val
+void List_destroy(List *list);  // Clear ListNode in List
+void List_clear_destroy (List *list);    //Clear all of ListNode
+
+
+//add
+void *List_push_head(List *list, void *val);
+void *List_push(List *list, void *val);
+
+//delete
+void *List_pop_head(List *list);
+void *List_pop(List *list);
+void *List_remove(List *list, ListNode *node);
+
+//TODO: modify
+//TODO: find
+
+//正向遍历
+#define LIST_FOREACH(LIST, START, V, next) for (ListNode *V = START; V != NULL; V = V->next)
+#endif
