3faf740b96fa2cd50cf94a9a3f766e2f649a7f78
tracer-ics2024 <tracer@njuics.org>
2025-02-17 15:51:34 +0800

    >  compile NEMU 231220000 张三 Linux ubutnu22 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:34 up 15:07,  5 users,  load average: 0.11, 0.21, 0.27

--- PA/nemu/src/monitor/sdb/expr.c
+++ PA/nemu/src/monitor/sdb/expr.c
@@ -81,14 +81,14 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[1024] __attribute__((used)) = {};
+static Token tokens[2048] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
-
+  memset(tokens, 0, sizeof(tokens));
   nr_token = 0;
 
   while (e[position] != '\0') {
