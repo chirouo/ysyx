25c67c26b960c4d23f40f7a139ab6f9db93024f5
tracer-ics2024 <tracer@njuics.org>
2025-02-18 10:49:14 +0800

    >  compile NEMU 231220000 张三 Linux ubutnu22 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:49:14 up  1:10,  1 user,  load average: 0.04, 0.37, 0.51

--- PA/nemu/src/monitor/sdb/watchpoint.c
+++ PA/nemu/src/monitor/sdb/watchpoint.c
@@ -99,10 +99,10 @@ void wp_display(){
     return;
   }
   WP* cur = head;
-  printf("Num\tType\t\tWhat\tOldValue\tNewValue\n");
+  printf("Num\tType\t\tWhat\t\tOldValue\tNewValue\n");
   while(cur->next != NULL)
   {
-    printf("%d\twatchpoint\t%s\t%u\t\t%u\n", cur->NO, cur->expr, cur->old, cur->new);
+    printf("%d\twatchpoint\t%s\t\t%u\t\t%u\n", cur->NO, cur->expr, cur->old, cur->new);
     cur = cur->next;
   }
 }
